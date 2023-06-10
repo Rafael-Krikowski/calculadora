c8a6e765fc0237849d0b75eb44e3385a2f53a293
rafael krikowski <krikowskirafael@gmail.com>
2023-06-10 00:17:52 -0300

    delimitacao de parenteses

--- calculo.js
+++ calculo.js
@@ -1,6 +1,7 @@
 var visor = document.getElementById('visor')
 var expressoes = []
 var operacoes = ['+', '-', '*', '/']
+var operacoesEspeciais = ['^(', '\u221A' + '(', '!(', 'log(']
 var expressoesTexto = ''
 
 
@@ -14,27 +15,33 @@ function inserirValor(){
     var ultimoCaractere = expressoes.slice(-1)[0]
     var penultimoCaractere = expressoes.slice(-2)[0]
 
-    if( operacoes.includes(penultimoCaractere) &&
-        operacoes.includes(ultimoCaractere)){
             if(penultimoCaractere == '*' && ultimoCaractere == '*'){
-                ultimoCaractere = '^('
+                ultimoCaractere = '^'
+                expressoes.pop()
+                expressoes[expressoes.length - 1] = ultimoCaractere
+                expressoes.push('(')
             }
 
             if(penultimoCaractere == '/' && ultimoCaractere == '/'){
-                ultimoCaractere = '\u221A' + '('
+                ultimoCaractere = '\u221A'
+                expressoes.pop()
+                expressoes[expressoes.length - 1] = ultimoCaractere
+                expressoes.push('(')
             }
 
             if(penultimoCaractere == '+' && ultimoCaractere == '+'){
-                ultimoCaractere = '!('
+                ultimoCaractere = '!'
+                expressoes.pop()
+                expressoes[expressoes.length - 1] = ultimoCaractere
+                expressoes.push('(')
             }
 
             if(penultimoCaractere == '-' && ultimoCaractere == '-'){
-                ultimoCaractere = 'log('
+                ultimoCaractere = 'log'
+                expressoes.pop()
+                expressoes[expressoes.length - 1] = ultimoCaractere
+                expressoes.push('(')
             }
-            
-            expressoes.pop()
-            expressoes[expressoes.length - 1] = ultimoCaractere
-    }
 
     expressoesTexto = expressoes.join('')
     visor.value = expressoesTexto
@@ -53,7 +60,62 @@ function limpar(){
 }
 
 function calcular(){
-    console.log(expressoes)
+    var indices = []
+    var base = []
+    var expoente = []
+
+    for(var i = 0; i < expressoes.length; i++){
+        if(expressoes[i] == '^'){
+            indices.push(i)
+        }
+    }
+
+    if(expressoes[indices[0] - 1] == ')'){
+        var indiceInicio = organizarParenteses(indices[0] - 1)
+        var indiceFinal = indices[0] - 1
+
+        console.log('inicio: ' + indiceInicio + ' | ' + 'fim: ' + indiceFinal)
+    }
 }
+
+function organizarParenteses(valor){
+    var parenteses = []
+    var indicesParenteses = []
+
+    for(var i = 0; i < expressoes.length; i++){
+        if(expressoes[i] == '(' || expressoes[i] == ')'){
+            parenteses.push(expressoes[i])
+            indicesParenteses.push(i)
+        }
+    }
+
+    var cont = 1
+    var indiceElemento = indicesParenteses.indexOf(valor)
+    var indiceProcurado = []
+
+    for(var i = indiceElemento - 1; i >= 0; i--){
+        if(parenteses[i] == ")"){
+            cont++
+        }
+        else{
+            cont--
+        }
+
+        if(cont == 0){
+            indiceProcurado.push(indicesParenteses[i])
+        }
+
+        console.log('cont: ' + cont)
+    }
+
+    return indiceProcurado[0]
+
+
+
+    console.log('indice: ' + indicesParenteses)
+    console.log(parenteses)
+    console.log('indices procurados:' + indiceProcurado)
+}
+
 
 
